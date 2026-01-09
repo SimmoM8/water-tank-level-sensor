7c18dc27201fff7bff9b7c3ed437b9e544a53e28
SimmoM8 <benjaminsimmons@outlook.com.au>
2026-01-10 10:47:36 +1100

    fix(config): update probe disconnected threshold and improve OTA password handling

--- water_level_sensor/water_level_config.h
+++ water_level_sensor/water_level_config.h
@@ -2,7 +2,7 @@
 // Copy any of the defines below to change probe reliability thresholds.
 // This file contains no secrets and is safe to commit (or add to .gitignore if preferred).
 
-#define CFG_PROBE_DISCONNECTED_BELOW_RAW 25000u
+#define CFG_PROBE_DISCONNECTED_BELOW_RAW 10000u
 #define CFG_CAL_MIN_DIFF 20u
 // #define CFG_SPIKE_DELTA 10000u
 // #define CFG_RAPID_FLUCTUATION_DELTA 5000u
